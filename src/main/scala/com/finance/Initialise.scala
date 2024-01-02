55b61b606ac6f5a918b70c8c2a7d17f6f702fe5a
adtsiko <anesu.tsiko@ovo.com>
2024-01-02 14:17:59 +0000

    file path fix

--- src/main/scala/com/finance/Initialise.scala
+++ src/main/scala/com/finance/Initialise.scala
@@ -13,41 +13,41 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Initialise {
 
-    val config = ConfigFactory.load()
-    private val dbHost = config.getString("db.host")
-    private val dbName = config.getString("db.db")
-    private val dbUsername = config.getString("db.username")
-    private val dbPassword = config.getString("db.password")
-    private val dbPort = config.getInt("db.port")
+  val config = ConfigFactory.load()
+  private val dbHost = config.getString("db.host")
+  private val dbName = config.getString("db.db")
+  private val dbUsername = config.getString("db.username")
+  private val dbPassword = config.getString("db.password")
+  private val dbPort = config.getInt("db.port")
 
-    given client: Resource[IO, Client[IO]] = EmberClientBuilder.default[IO].build
+  given client: Resource[IO, Client[IO]] = EmberClientBuilder.default[IO].build
 
-    given transactor: Resource[IO, HikariTransactor[IO]] =
-      for {
-        ce <- ExecutionContexts.fixedThreadPool[IO](16) // our connect EC
-        xa <- HikariTransactor.newHikariTransactor[IO](
-          "org.postgresql.Driver",
-          s"jdbc:postgresql://$dbHost:$dbPort/$dbName",
-          dbUsername,
-          dbPassword,
-          ce
-        )
-      } yield xa
+  given transactor: Resource[IO, HikariTransactor[IO]] =
+    for {
+      ce <- ExecutionContexts.fixedThreadPool[IO](16) // our connect EC
+      xa <- HikariTransactor.newHikariTransactor[IO](
+        "org.postgresql.Driver",
+        s"jdbc:postgresql://$dbHost:$dbPort/$dbName",
+        dbUsername,
+        dbPassword,
+        ce
+      )
+    } yield xa
 
-    given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
 
-     def initialiseDb()(using db: Resource[IO, HikariTransactor[IO]]): IO[Unit] = {
+  def initialiseDb()(using db: Resource[IO, HikariTransactor[IO]]): IO[Unit] = {
 
-      db.use {
+    db.use {
 
-        xa =>
-          for {
-            _ <- IO(println("Initialising DB......"))
-            sqlFileContent <- IO(Source.fromResource("sql/initialisation.sql").mkString)
-            sqlString = Fragment.const(sqlFileContent).update
-            _ <- sqlString.run.transact(xa)
-            _ <- IO(println("DB Initialisation complete..."))
+      xa =>
+        for {
+          _ <- IO(println("Initialising DB......"))
+          sqlFileContent <- IO(Source.fromResource("sql/initialisation.sql").mkString)
+          sqlString = Fragment.const(sqlFileContent).update
+          _ <- sqlString.run.transact(xa)
+          _ <- IO(println("DB Initialisation complete..."))
         } yield ()
     }
   }
